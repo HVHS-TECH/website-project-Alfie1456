e6d5acbad1e919d0cc1e0d14c57825d6d2462c54
Alfie1456 <161382522+Alfie1456@users.noreply.github.com>
2025-06-16 22:58:10 +0000

    Adding simple layout to all car info pages

--- carSelection.js
+++ carSelection.js
@@ -1,23 +1,4 @@
-const toggleSearch = () => {
-  const searchForm = document.querySelector('.search-form');
-  const searchButton = document.querySelector('.search-button');
-  const searchInput = document.querySelector('.search-input');
 
-  searchButton.addEventListener('click', () => {
-    searchForm.classList.toggle('active-search');
-  });
-
-  searchInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      searchInput.value = '';
-      searchForm.classList.remove('active-search');
-      console.log()
-    }
-  });
-};
-
-toggleSearch();
 
 filterSelection("all")
 function filterSelection(c) {
